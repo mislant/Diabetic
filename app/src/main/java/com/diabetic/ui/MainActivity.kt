8aa7f9b9321eb5f9fb13b3ab9f43b7947f943d60
mislant <kiryakz2000@gmail.com>
2023-12-17 18:55:32 +0600

    feat: init base android application with clean arch directory structure

---
+++ app/src/main/java/com/diabetic/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.diabetic.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
