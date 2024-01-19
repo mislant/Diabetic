fd55a9848831e277e6e67396736bef92a26e0a97
mislant <kiryakz2000@gmail.com>
2024-01-20 01:20:41 +0600

    refactor: remove useless imports

--- app/src/main/java/com/diabetic/ui/screen/main/component/GlucoseLevelTable.kt
+++ app/src/main/java/com/diabetic/ui/screen/main/component/GlucoseLevelTable.kt
@@ -1,6 +1,5 @@
 package com.diabetic.ui.screen.main.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +11,6 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.diabetic.domain.model.GlucoseLevel
